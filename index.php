6687258a6293babae02dd4b88b56420a27298edc
Alex <02frasera@gmail.com>
2020-06-14 17:46:48 +1000

    Started the create link function

--- index.php
+++ index.php
@@ -5,60 +5,46 @@ if (Session::exists('home')) {
 }
 
 
+
 $user = new User();
 if ($user->isLoggedIn()) {
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
     <link rel="stylesheet" href="css/style.css">
-</head>
-<p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!</p>
-<ul>
-  <li><a href="update.php">Update Details</a></li>
-  <li><a href="changepassword.php">Change Password</a></li>
-  <li><a href="logout.php">Logout</a></li>
-</ul>
+    <script src="javascript/script.js" type="text/javascript">
+    </script>
 <body>
-    <header>
-        <nav>
-            <div class="center">
-                <a href="index.html" class="centre-links">Home</a>
-                <a href="#about" class="centre-links">About</a>
-                <a href="#contact" class="centre-links">Contact us</a>
-            </div>
-            <div class="floated-links">
-                <a href="signup.php" class="centre-links">Signup</a>
-                <a href="login.php" class="centre-links">Login</a>
-            </div>
-        </nav>
-        <div class="main-title">
-            <h1>Webscape</h1>
-            <p>Find the web developer for you...</p>
-            <a href="signup.php"><strong>Signup now!!</strong></a>
-        </div>
-    </header>
-    <main>
-        <div class="wrapper">
-            <div id="about">
-                <h2>About Webscape</h2>
-                <img src="" alt="">
-                <p></p>
-            </div>
-            <div id="contact">
+  <div class="logo">Logo</div>
+  <div class="nav">Nav
+    <p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!</p>
+    <ul>
+      <li><a href="update.php">Update Details</a></li>
+      <li><a href="changepassword.php">Change Password</a></li>
+      <li><a href="logout.php">Logout</a></li>
+      <li><a href="createlink.php">Create Link</a></li>
+    </ul>
+  </div>
+  <main>
+    <div class="featured" id="featured">Featured
+    </div>
+    <div class="search">
+      <div class="filterbox">
 
-            </div>
-        </div>
-    </main>
-    <footer>
-        <p>2020 &copy All Rights Reserved. <a href="#" class="footer-links">Privacy Policy</a> | <a href="#" class="footer-links">Terms of Use</a> | <a href="#" class="footer-links">License</a> | <a href="#" class="footer-links">Support</a></p>
-    </footer>
+      </div>
+      <input type="text" class="searchbar" placeholder="What are you looking for..."></input>
+      <div class="results" id="results">
+      </div>
+    </div>
+  </main>
+  <div class="footer">Footer</div>
 </body>
-</html>
 
 <?php
 	} else {
---
+++ javascript/script.js
@@ -0,0 +1,39 @@
+//Creates "result" that displays a hash
+function addResult(ammount, location, name, hash, likes, dislikes) { //addResult(3, "featured", "Batman.mp4", "h35g2uy5t23v5v3t2yu52", 10, 1)
+  for (var i = 1; i < ammount+1; i++) {
+    var resultLocation = document.getElementById(location); //Location
+
+    //Main div
+    var resultDiv = document.createElement("div");
+    resultDiv.classList.add("result");
+    resultDiv.id = "result" + location + i;
+    resultLocation.appendChild(resultDiv);
+
+    //Info Div
+    var hashInfoDiv = document.createElement("div");
+    hashInfoDiv.classList.add("hash-info");
+    hashInfoDiv.id = "hash-info" + location + i;
+    resultDiv.appendChild(hashInfoDiv);
+
+    //Name Div
+    var nameDiv = document.createElement("div");
+    nameDiv.classList.add("name");
+    nameDiv.id = "name" + location + i;
+    nameDiv.innerHTML = name; //Retrive from DB
+    hashInfoDiv.appendChild(nameDiv);
+
+    //Hash Div
+    var hashDiv = document.createElement("div");
+    hashDiv.classList.add("hash");
+    hashDiv.id = "hash" + location + i;
+    hashDiv.innerHTML = hash; //Retrive from DB
+    hashInfoDiv.appendChild(hashDiv);
+
+    //Score Div
+    var scoreDiv = document.createElement("div");
+    scoreDiv.classList.add("score");
+    scoreDiv.id = "score" + location + i;
+    scoreDiv.innerHTML = likes + "/" + dislikes; //Retrive from DB
+    resultDiv.appendChild(scoreDiv);
+  }
+}
