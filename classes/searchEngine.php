a38ddc2332fb59e61edecbd54c68f638cd897b70
Alex <02frasera@gmail.com>
2020-07-27 18:23:20 +1000

    Search engine can order by searchScore now to order by vote

---
+++ classes/searchEngine.php
@@ -0,0 +1,84 @@
+<?php //Add exception errors
+  class SearchEngine {
+    private $_db,
+            $_comparedIds = array(0), //Array of comapred ID's
+            $_resultsIds = array(0);
+
+    public function __construct() {
+      $this->_db 			= Database::getInstance();
+    }
+
+    public function search($searchTerms) {
+      $databaseResults = array();
+      for ($i=0; $i < count($searchTerms); $i++) {
+        if (!$searchTerms[$i] == "") {
+          $databaseResults[] = $this->_db->search('links', array("name", "hash", "file_extension", "created"), $searchTerms[$i], "%%");
+          $databaseResults[$i] = $databaseResults[$i]->results();
+        }
+      }
+      $tallyedResults = $this->tallyResults($databaseResults);
+      $talleyedResults = $this->removeDuplicates($tallyedResults);
+      $tallyedResultsInOrder = $this->orderResults($tallyedResults);
+      return $tallyedResultsInOrder;
+    }
+
+    public function tallyResults($databaseResults = array()) {
+      if (false) { //Do a check if there is only one result
+        return $databaseResults;
+      }
+      for ($a=0; $a < count($databaseResults); $a++) { //Database result Main
+        for ($b=0; $b < count($databaseResults[$a]); $b++) { //Specific database result
+          $needle = $databaseResults[$a][$b]->id; //Neddle in haystack? Makes it 15x faster
+          $flipped_haystack = array_flip($this->_comparedIds);  //https://www.php.net/manual/en/function.in-array.php
+          if (isset($flipped_haystack[$needle])) { //Checks if value has already been checked to avoid duplicates //if (in_array($databaseResults[$a][$b]->id, $this->_comparedIds[])) { may also work
+            continue; //The reason for not using a ! is because that was not working
+          }
+          $this->_comparedIds[] = $databaseResults[$a][$b]->id; //Adds ID into array
+          $databaseResults[$a][$b]->searchScore = 0; //Initialise results search score
+          for ($c=0; $c < count($databaseResults); $c++) { //Database result comparison
+            if (!$a == $c) { //Makes sure results arent compared with themselves
+              for ($d=0; $d < count($databaseResults[$c]); $d++) { //Specific result to compare
+                if ($databaseResults[$a][$b]->id == $databaseResults[$c][$d]->id) { //Compares results ID's
+                  $databaseResults[$a][$b]->searchScore += 1; //Increases search score
+                }
+              }
+            }
+          } //Batman Dark
+        }
+      }
+      return $databaseResults;
+    }
+
+    public function removeDuplicates($tallyedResults = array()) { //Puts all in one array and removes duplicates
+      $duplicateFree = array();
+      for ($a=0; $a < count($tallyedResults); $a++) {
+        for ($b=0; $b < count($tallyedResults[$a]); $b++) {
+          $needle = $tallyedResults[$a][$b]->id; //Neddle in haystack? Makes it 15x faster //May make it slower
+          $flipped_haystack = array_flip($this->$_resultsIds);  //https://www.php.net/manual/en/function.in-array.php
+          if (isset($flipped_haystack[$needle])) {
+            continue;
+          }
+          $this->$_resultsIds[] = $tallyedResults[$a][$b]->id;
+          $duplicateFree[] = $tallyedResults[$a][$b];
+        }
+      }
+      return $duplicateFree;
+    }
+
+    public function orderResults($tallyedResults = array()) { //Orders by search score and votes
+      $tallyedResultsInOrder = array();
+      for ($i=0; $i < count($tallyedResults); $i++) {
+        $searchScoreLargestIndex = 0;
+        for ($a=0; $a < count($tallyedResults); $a++) {
+          if ($tallyedResults[$a]->searchScore > $tallyedResults[$searchScoreLargestIndex]->searchScore) {
+            $searchScoreLargestIndex = $a;
+          }
+        }
+        $tallyedResultsInOrder[$i] = $tallyedResults[$searchScoreLargestIndex];
+      }
+      return $tallyedResultsInOrder;
+    }
+
+  }
+
+ ?>
--- search.php
+++ search.php
@@ -2,21 +2,14 @@
   require_once 'core/init.php';
 
   if (Input::exists()) { //Perhaps this code should go in "Link.php" but that would cause the database to be searched every time a link is created. Maybe create a new file for it
-    $_db = Database::getInstance();
+    $searchEngine = new SearchEngine();
     $searchTerms = explode(' ', Input::get('search')); //Remove all spaces from array
-    for ($i=0; $i < count($searchTerms); $i++) {
-      if (!$searchTerms[$i] == "") {
-        $databaseResults[] = $_db->search('links', array("name", "hash", "file_extension", "created"), $searchTerms[$i], "%%");
-        $databaseResults[$i] = $databaseResults[$i]->results();
-      }
+    try {
+      $results = $searchEngine->search($searchTerms);
+      //Make $results paletable for ajax
+      return $results;
+    } catch (\Exception $e) {
+      die($e->getMessage());
     }
-    foreach ($databaseResults as $$databaseResult) {
-
-    }
-    // for ($i=0; $i < ; $i++) {
-    //   $links[] = new Link($i, "results_search", Input::get('search'));
-    // }
-
-
   }
  ?>
